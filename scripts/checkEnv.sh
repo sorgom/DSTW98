bc1a4c5267eb3a012323abebcf286bcfd995f7b4
Your Name <you@example.com>
2025-04-05 14:55:10 +0000

    envCheck

--- scripts/checkEnv.sh
+++ scripts/checkEnv.sh
@@ -3,6 +3,13 @@
 #   check for availability of tools
 #   ====================================
 
-for call in python python3 py gcov cppcheck; do
+for call in python python3 py gcov cppcheck valgrind; do
     printf "%-12s : %s\n" $call $(which $call)
+done
+echo python?
+for call in python python3; do
+    if [ ! -z $(which $call) ]; then
+        echo $call
+        $call --version
+    fi
 done
